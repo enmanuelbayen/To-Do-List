9641ba7ac9f8421af417b17f81ae898afb1ff160
enmanuelbayen <enmanuel0495@gmail.com>
2023-05-02 17:26:13 -0400

    CSS section

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -9,7 +9,24 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: './src/index.html',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  
 };
